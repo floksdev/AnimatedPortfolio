5f2106f30a25005e0fdd500a583097d2fbf4e05c
floksdev <tristan.wehrle@epitech.eu>
2025-02-25 16:19:21 +0100

    [ADD] navbar animation mobile

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,35 +1,34 @@
 import Tag from "@/components/Tag";
-import figmaIcon from '@/assets/images/figma-logo.svg'
-import notionIcon from '@/assets/images/notion-logo.svg'
-import slackIcon from '@/assets/images/slack-logo.svg'
-import relumeIcon from '@/assets/images/relume-logo.svg'
-import framerIcon from '@/assets/images/framer-logo.svg'
-import githubIcon from '@/assets/images/github-logo.svg'
+import Image from "next/image";
+import figmaIcon from "@/assets/images/figma-logo.svg";
+import notionIcon from "@/assets/images/notion-logo.svg";
+import slackIcon from "@/assets/images/slack-logo.svg";
+import relumeIcon from "@/assets/images/relume-logo.svg";
+import framerIcon from "@/assets/images/framer-logo.svg";
+import githubIcon from "@/assets/images/github-logo.svg";
+import IntegrationColumn from "@/components/IntegrationsColumn";
 
 const integrations = [
-    { name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-    { name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-    { name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
+    { name: "Figma", icon: figmaIcon, description: "Figma is a collaborative interface design tool." },
+    { name: "Notion", icon: notionIcon, description: "Notion is an all-in-one workspace for notes and docs." },
+    { name: "Slack", icon: slackIcon, description: "Slack is a powerful team communication platform." },
+    { name: "Relume", icon: relumeIcon, description: "Relume is a no-code website builder and design system." },
+    { name: "Framer", icon: framerIcon, description: "Framer is a professional website prototyping tool." },
+    { name: "GitHub", icon: githubIcon, description: "GitHub is the leading platform for code collaboration." },
 ];
+
+export type IntegrationType = typeof integrations;
 
 export default function Integrations() {
     return (
-        <section>
+        <section className="py-24 overflow-hidden">
             <div className="container">
                 <Tag>Integrations</Tag>
-                <h2>Plays well with <span>others</span></h2>
-                <p>Layers blablabla bla</p>
-                <div>
-                    {integrations.map (integration => (
-                        <div key={integration.name}>
-                            <div></div>
-                            <h3>{integration.name}</h3>
-                            <p>{integration.description}</p>
-                        </div>
-                    ))}
+                <h2 className="text-6xl font-medium mt-6">Plays well with <span className="text-lime-400">others</span></h2>
+                <p className="text-white/50 mt-4 text-lg">Layers blablabla bla</p>
+                <div className="h-[400px] mt-8 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+                    <IntegrationColumn integrations={integrations}/>
+                    <IntegrationColumn integrations={integrations.slice().reverse()} className="hidden md:flex"/>
                 </div>
             </div>
         </section>
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { section } from "framer-motion/client";
 import Image from "next/image";
 import logoImage from '@/assets/images/logo.svg'
 import menuImage from '@/assets/images/menu.min.svg'
 import Button from "@/components/Button";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -12,9 +17,11 @@ const navLinks = [
 ];
 
 export default function Navbar() {
-    return <section className="py-4 px-4 lg:py-8 lg:px-8">
-        <div className="container max-w-5xl"></div>
-            <div className="grid grid-cols-2 border lg:grid-cols-3 border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+    const [isOpen, setIsOpen] = useState(false);
+    return <section className="py-4 px-4 lg:py-8 lg:px-8 fixed w-full top-0 z-50">
+        <div className="container max-w-5xl">
+            <div className="border border-white/15 rounded-[27px] bg-neutral-950/70 backdrop-blur">
+                <div className="grid grid-cols-2 border lg:grid-cols-3 border rounded-full border-white/15 p-2 px-4 md:pr-2 items-center">
             <div>
                 <Image src={logoImage} alt="Layers logo" className="h-9 md:h-auto w-auto"/>
             </div>
@@ -36,11 +43,27 @@ export default function Navbar() {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     className="feather feather-menu md:hidden"
+                    onClick={() => setIsOpen(!isOpen)}
                     >
-                        <path d="M3 12h18M3 6h18M3 18h18"/>
+                    <line x1="3" y1="6" x2="21" y2="6" className={twMerge("origin-left transition", isOpen && 'rotate-45 -translate-y-1')}></line>
+                    <line x1="3" y1="12" x2="21" y2="12" className={twMerge("transition", isOpen && "opacity-0")}></line>
+                    <line x1="3" y1="18" x2="21" y2="18" className={twMerge("origin-left transition", isOpen && '-rotate-45 translate-y-1')}></line>
                 </svg>
                 <Button className="hidden md:inline-flex items-center" variant="secondary">Log in</Button>
                 <Button className="hidden md:inline-flex items-center" variant="primary">Sign up</Button>
+            </div>
+                </div>
+                <AnimatePresence>
+                {isOpen && (
+                <motion.div initial={{height: 0}} animate={{height: 'auto'}} exit={{height: 0}} className="flex flex-col items-center gap-4 py-4 overflow-hidden">
+                    {navLinks.map(link => (
+                        <a href={link.href} key={link.label} className="py-2">
+                            {link.label}
+                        </a>
+                    ))}
+                </motion.div>
+                )}
+                </AnimatePresence>
             </div>
         </div>
     </section>;
