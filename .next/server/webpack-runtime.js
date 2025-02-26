55f112cc79fe03d65c83df4b3bc6a01e47be0564
floksdev <tristan.wehrle@epitech.eu>
2025-02-27 00:16:01 +0100

    [FIX] navbar && custom hero section

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("e0dded9e41a3a4d8")
+/******/ 		__webpack_require__.h = () => ("02cf4268be1b177d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,17 +13,17 @@ export default function Hero() {
                     <Image className="w-25" src={Pic}></Image>
                     <div className="py-1 px-3 bg-gradient-to-r from-violet-500 to-purple-700 rounded-full text-white font-bold">✨ Qui-suis-je ?</div>
                 </motion.div>
-                <h1 className="text-6xl md:text-6xl font-medium text-center mt-6">
+                <motion.h1 initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} className="text-6xl md:text-6xl font-medium text-center mt-6">
                     Curieux et innovant, je vous<br />
                     invite à découvrir un univers<br />
                     où chaque projet est une <br />
                     aventure en devenir
-                </h1>
-                <p className="text-center text-xl text-white/50 mt-8 max-w-3xl mx-auto">
+                </motion.h1>
+                <motion.p initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} className="text-center text-xl text-white/50 mt-8 max-w-3xl mx-auto">
                     Passionné depuis toujours par la technologie et le graphisme, j’ai choisi la <br />
                     programmation pour donner vie à mes idées débordantes. Actuellement étudiant <br/ >
                     en 3ème année à Epitech Paris, je repousse sans cesse les limites de ma créativité.
-                </p>
+                </motion.p>
             </div>
         </section>
     );
