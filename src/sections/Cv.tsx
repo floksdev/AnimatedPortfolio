82eef97530eca0449a3bf7ef56633bb11b4b45eb
floksdev <tristan.wehrle@epitech.eu>
2025-02-28 00:48:47 +0100

    [ADD] CV inside scroll animation && update text

--- src/sections/Cv.tsx
+++ src/sections/Cv.tsx
@@ -1,6 +1,9 @@
 import Tag from "@/components/Tag";
 import { ContainerScroll } from "@/components/ContainerScrollAnimation";
+import { Button }
 import TitleComponent from "@/components/TitleComponent";
+import CvPreview from "@/assets/images/CvPreview.svg"
+import Image from "next/image";
 
 export default function Cv() {
     return (
@@ -8,12 +11,17 @@ export default function Cv() {
             <div className="container">
                 <div className="flex justify-center items-center flex-col">
                     <Tag>Mon CV</Tag>
-                    <ContainerScroll titleComponent={<TitleComponent className="text-6xl font-bold text-white">Envie de continuer<br /> l'aventure ?</TitleComponent>}>
-                        
-                            <p className="text-white text-lg">
-                                Voici mon CV interactif avec un effet de défilement animé.
-                            </p>
+                    <ContainerScroll titleComponent={<TitleComponent className="text-6xl font-bold text-white">Envie de continuer<br /><span className="text-violet-600">l'aventure</span> ?</TitleComponent>}>
+                    <div className="relative w-full h-full rounded-lg overflow-hidden">
+                        <Image
+                            alt="Cv Preview"
+                            src={CvPreview}
+                            fill
+                            style={{ objectFit: "cover" }}
+                        />
+                    </div>
                     </ContainerScroll>
+                    <Button></Button>
                 </div>
             </div>
         </section>
