cde32f571e6c16d4bd257cb13f561485f02ccf44
floksdev <tristan.wehrle@epitech.eu>
2025-03-11 19:20:58 +0100

    [ADD] hero section responsiveness

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
     <footer className="py-6 bg-neutral-950 border-t-4 border-violet-600">
       <div className="container mx-auto px-4 flex flex-col items-center space-y-4">
         <p className="text-center text-sm text-gray-400 font-extralight">
-            Portfolio réalisé avec passion avec Next.js, Tailwind CSS et Framer Motion
+            Portfolio réalisé avec passion en Next.js, Tailwind CSS et Framer Motion
         </p>
         <motion.a
           href="https://fr.trustpilot.com/evaluate/tristan-wehrle.com"
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -58,34 +58,52 @@ export default function Hero() {
           >
             ✨ Qui-suis-je ?
           </motion.div>
-
         </motion.div>
 
         <motion.h1
           variants={titleVariants}
           initial="hidden"
           animate="visible"
-          className="text-6xl md:text-6xl font-medium text-center mt-6"
+          className="hidden lg:block text-3xl sm:text-4xl md:text-6xl font-medium text-center mt-6"
         >
           Curieux et innovant, je vous
-          <br />
+          <br className="hidden md:block" />
           invite à découvrir un univers
-          <br />
+          <br className="hidden md:block" />
           où chaque projet est une
-          <br />
+          <br className="hidden md:block" />
           aventure en devenir
+        </motion.h1>
+
+        <motion.h1
+          variants={titleVariants}
+          initial="hidden"
+          animate="visible"
+          className="block lg:hidden text-3xl sm:text-4xl md:text-6xl font-medium text-center mt-6"
+        >
+          Curieux et innovant, je vous invite à découvrir un univers où chaque projet est une aventure en devenir
         </motion.h1>
 
         <motion.p
           variants={itemVariants}
           initial="hidden"
           animate="visible"
-          className="text-center text-xl text-white/50 mt-8 max-w-3xl mx-auto"
+          className="hidden lg:block text-center text-xl text-white/50 mt-8 max-w-3xl mx-auto"
         >
           Passionné depuis toujours par la technologie et le graphisme, j’ai choisi la{" "}
           <br />
           programmation pour donner vie à mes idées débordantes. Actuellement étudiant{" "}
           <br />
+          en 3ème année à Epitech Paris, je repousse sans cesse les limites de ma créativité.
+        </motion.p>
+        <motion.p
+          variants={itemVariants}
+          initial="hidden"
+          animate="visible"
+          className="block lg:hidden text-center text-xl text-white/50 mt-8 max-w-3xl mx-auto"
+        >
+          Passionné depuis toujours par la technologie et le graphisme, j’ai choisi la
+          programmation pour donner vie à mes idées débordantes. Actuellement étudiant
           en 3ème année à Epitech Paris, je repousse sans cesse les limites de ma créativité.
         </motion.p>
       </div>
